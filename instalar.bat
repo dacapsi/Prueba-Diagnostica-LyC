eba26af7a7b58087b61efd1edb635f2d8e99980e
Carlos Martinez <121403628+dacapsi@users.noreply.github.com>
2026-05-03 15:20:34 -0400

    Add files via upload

---
+++ instalar.bat
@@ -0,0 +1,6 @@
+@echo off
+echo --- Instalando dependencias para Prueba Diagnostica UNEG ---
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+echo --- Instalacion finalizada con exito ---
+pause
---
+++ instalar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Instalando dependencias para Prueba Diagnóstica UNEG..."
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
+echo "Instalación finalizada."
